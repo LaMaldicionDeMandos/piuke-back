526c6944519e54cccd807cb384fb74262893c9c3
marcelo <pasutmarcelo@gmail.com>
2021-08-24 16:16:55 -0300

    agrego cors para soportar cross origin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const healthRouter = require('./routes/health.router');
 const productsRouter = require('./routes/products.router');
@@ -12,6 +13,7 @@ const app = express();
 
 console.log('Enviroment => ' + process.env.ENV);
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
