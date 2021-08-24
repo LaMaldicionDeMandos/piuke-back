c9b2a8560d156c903e3a76ff1ed11b80bb7662a8
marcelo <pasutmarcelo@gmail.com>
2021-08-24 15:32:15 -0300

    agrego partial response

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const meliService = require('../services/meli.service');
+const keepPropertiesAfter = require('./keepPropertiesAfter');
 
-router.get('', (req, res, next) => {
+router.get('', [keepPropertiesAfter('id,title,price,available_quantity,sold_quantity,start_time,thumbnail,status')],(req, res) => {
     meliService.getProducts()
         .then(products => res.send(products))
         .catch(e => res.status(400).send());
