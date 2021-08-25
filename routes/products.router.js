22b1168f80f3bc11e43b2303d44e59eac250880b
marcelo <pasutmarcelo@gmail.com>
2021-08-25 13:20:36 -0300

    Agrego post de producto y valido que esté code y cost

--- routes/products.router.js
+++ routes/products.router.js
@@ -2,11 +2,30 @@ const express = require('express');
 const router = express.Router();
 const meliService = require('../services/meli.service');
 const keepPropertiesAfter = require('./keepPropertiesAfter');
+const { body, validationResult } = require('express-validator');
+
+errorMiddleware = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    next();
+};
 
 router.get('', [keepPropertiesAfter('id,title,price,available_quantity,sold_quantity,start_time,thumbnail,status')],(req, res) => {
     meliService.getProducts()
         .then(products => res.send(products))
         .catch(e => res.status(400).send());
+});
+
+router.post('',
+    body('code').not().isEmpty(),
+    body('cost').not().isEmpty(),
+    body('cost').isNumeric(),
+    errorMiddleware,
+    (req, res) => {
+    console.log(`Nuevo producto => ${JSON.stringify(req.body)}`);
+    res.sendStatus(201);
 });
 
 module.exports = router;
