6e425b00b24869531403e678922609930c32eff7
marcelo <pasutmarcelo@gmail.com>
2021-08-30 15:31:52 -0300

    agrego listing type

--- routes/products.router.js
+++ routes/products.router.js
@@ -12,7 +12,16 @@ errorMiddleware = (req, res, next) => {
     next();
 };
 
-router.get('', [keepPropertiesAfter('id,meli_items(title,price,available_quantity,sold_quantity,start_time,thumbnail,status),code,cost')],(req, res) => {
+router.get('', [keepPropertiesAfter('id,meli_items(title,price,available_quantity,sold_quantity,start_time,thumbnail,status,listing_type_id),code,cost')],(req, res) => {
+    productsService.getAllProducts()
+        .then(products => {
+            console.log("Products: " + JSON.stringify(products));
+            res.send(products)
+        })
+        .catch(e => res.status(400).send());
+});
+
+router.get('/full', (req, res) => {
     productsService.getAllProducts()
         .then(products => {
             console.log("Products: " + JSON.stringify(products));
