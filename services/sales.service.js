d1856d31075b1d48075c981240001da5e551d067
marcelo <pasutmarcelo@gmail.com>
2021-10-19 16:40:46 -0300

    saco logs

--- services/sales.service.js
+++ services/sales.service.js
@@ -48,11 +48,7 @@ class SalesService {
     #findByCode = (code) => ProductBase.findOne({where: {code: code}})
 
     #addProduct = (order) => {
-        console.log("===================== Busco el product base y no lo encuentro? seller_sku: " + order.item.seller_sku);
-        const basePromise = this.#findByCode(order.item.seller_sku).then(p => p.toJSON()).then((p) => {
-            console.log("========= Entonces no era acá? " + p.code);
-            return p;
-        });
+        const basePromise = this.#findByCode(order.item.seller_sku).then(p => p.toJSON());
         const meliItemPromise = meliService.getProductsById(order.item.id);
         return Promise.all([basePromise, meliItemPromise])
             .then(results => {
